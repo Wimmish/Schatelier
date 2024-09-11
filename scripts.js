1d887b677a8426e332871c6d153aaa17237ccd9e
Wim <Wimmish@users.noreply.github.com>
2024-09-11 14:02:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,24 @@
-// Get the modal
 var modal = document.getElementById("imageModal");
-
-// Get the modal content
 var modalContent = document.getElementById("modalContent");
-
-// Get the <span> element that closes the modal
 var closeButton = document.getElementsByClassName("close-button")[0];
-
-// Get all section-slider-item elements
 var sliderItems = document.querySelectorAll('.section-slider-item');
-
-// Get the main content container
 var mainContent = document.querySelector('.section-slider-container');
 
 // Add click event to all section-slider-item elements
 sliderItems.forEach(function(item) {
   item.onclick = function() {
     modal.style.display = "flex";
-    // Clone the content
+
     var clone = this.cloneNode(true);
     modalContent.innerHTML = ''; // Clear previous content
     modalContent.appendChild(clone);
-    mainContent.classList.add('blur'); // Add blur effect
+    mainContent.classList.add('blur');
   }
 });
 
-// Function to close modal and remove blur
 function closeModal() {
   modal.style.display = "none";
-  mainContent.classList.remove('blur'); // Remove blur effect
+  mainContent.classList.remove('blur'); 
 }
 
 // Close the modal when the close button is clicked
@@ -47,3 +37,18 @@ document.addEventListener('keydown', function(event) {
     closeModal();
   }
 });
+
+// Mobile nav
+const navToggle = document.querySelector('.nav__toggle');
+const navItems = document.querySelectorAll('.nav__item'); // Add this line
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
+
